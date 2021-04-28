95b55efd250311720421ac57709339650010aa25
Hayden Disley <haydendisley@Haydens-MacBook-Pro.local>
2021-04-28 17:28:49 +0100

    fetch requests now run synchronously

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const { google } = require("googleapis");
 const fetch = require("node-fetch");
 const { spreadsheetId } = require("./secrets");
 
+let visibilityScore = [];
 const updateSistrix = async () => {
-  console.log(spreadsheetId);
   const auth = new google.auth.GoogleAuth({
     keyFile: "credentials.json",
     scopes: "https://www.googleapis.com/auth/spreadsheets",
@@ -11,6 +11,7 @@ const updateSistrix = async () => {
   const client = await auth.getClient();
   const googleSheets = google.sheets({ version: "v4", auth: client });
   const metaData = await googleSheets.spreadsheets.get({ auth, spreadsheetId });
+  const endpoint = `https://api.sistrix.com/domain.sichtbarkeitsindex?api_key=LtADkhSEcRs5QrDrPxD84YMt2SjRUA2FgMrh&date=&04-26-2021&domain=`;
   const getRows = await googleSheets.spreadsheets.values
     .get({
       auth,
@@ -18,36 +19,49 @@ const updateSistrix = async () => {
       range: "allClientsData!B1:GZ1",
     })
     .then((res) => {
-      // Update date in api request --------------------------------------------------------------------------------------->           <-------
-      const endpoint = `https://api.sistrix.com/domain.sichtbarkeitsindex?api_key=LtADkhSEcRs5QrDrPxD84YMt2SjRUA2FgMrh&date=&04-26-2021&domain=`;
-      let visibilityScore = [];
-      res.data.values.flat().forEach((domain) => {
-        fetch(endpoint + domain + "&format=json")
-          .then((res) => res.json())
-          .then((res) => {
-            visibilityScore.push(
-              res.answer[0].sichtbarkeitsindex[0].value +
-                " " +
-                res.answer[0].sichtbarkeitsindex[0].domain
-            );
-          });
-      });
-      setTimeout(() => {
-        const parsedScore = visibilityScore.map((e) => e.toString());
-        console.log(parsedScore);
-        googleSheets.spreadsheets.values
-          .append({
-            auth,
-            spreadsheetId,
-            // Updata cell Range ->          <-
-            range: "allClientsData!B307:GZ307",
-            valueInputOption: "USER_ENTERED",
-            resource: {
-              values: [parsedScore],
-            },
-          })
-          .catch((err) => console.log(err));
-      }, 10000);
-    });
+      const the_data = res.data.values.flat();
+      const arrayOfPromises = the_data.map((url) => () =>
+        yourMainFunction(url, endpoint)
+      );
+      arrayOfPromises
+        .reduce(
+          (promise, func) =>
+            promise.then((result) =>
+              func().then(Array.prototype.concat.bind(result))
+            ),
+          Promise.resolve([])
+        )
+        .then((res) => {
+          const parsedScore = visibilityScore.map((e) => e.toString());
+          googleSheets.spreadsheets.values
+            .append({
+              auth,
+              spreadsheetId,
+              // Updata cell Range ->          <-
+              range: "allClientsData!B307:GZ307",
+              valueInputOption: "USER_ENTERED",
+              resource: {
+                values: [parsedScore],
+              },
+            })
+            .catch((err) => console.log(err));
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
 };
 updateSistrix();
+const yourMainFunction = (domain, endpoint) => {
+  return new Promise((resolve) => {
+    fetch(endpoint + domain + "&format=json")
+      .then((res) => res.json())
+      .then((res) => {
+        let value =
+          res.answer[0].sichtbarkeitsindex[0].value +
+          visibilityScore.push(value);
+        console.log(value);
+        resolve();
+      })
+      .catch((err) => console.log(err));
+  });
+};
